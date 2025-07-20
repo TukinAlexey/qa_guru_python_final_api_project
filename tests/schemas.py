c0462f3c417dd8072e08e8d9c23b7ec9423ab52c
Alexey Tukin <tukin2k@gmail.com>
2025-07-20 12:56:39 +0300

    add_api_tests

---
+++ tests/schemas.py
@@ -0,0 +1,75 @@
+post_register_successful = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "integer"
+    },
+    "token": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "id",
+    "token"
+  ]
+}
+
+post_register_unsuccessful = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "error": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "error"
+  ]
+}
+
+put_user_update = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "updatedAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "updatedAt"
+  ]
+}
+
+post_user_create = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "id": {
+      "type": "string"
+    },
+    "createdAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "id",
+    "createdAt"
+  ]
+}
---
+++ tests/test_api.py
@@ -0,0 +1,96 @@
+import requests
+from jsonschema import validate
+import schemas
+
+url = 'https://reqres.in'
+register_successful_endpoint = '/api/register'
+single_user_not_found_endpoint = '/api/users/23'
+user_update_endpoint = '/api/users/2'
+user_delete_endpoint = '/api/users/2'
+user_create_endpoint = '/api/users'
+
+def test_register_successful_response_status_code_with_all_params_in_body():
+    payload = {
+    "email": "eve.holt@reqres.in",
+    "password": "pistol"
+    }
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.post(url + register_successful_endpoint, json=payload, headers=headers)
+    assert response.status_code == 200
+
+def test_register_successful_response_status_code_with_not_all_params_in_body():
+    payload = {
+    "email": "eve.holt@reqres.in"
+}
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.post(url + register_successful_endpoint, json=payload, headers=headers)
+    body = response.json()
+    validate(body, schema=schemas.post_register_unsuccessful)
+    assert response.status_code == 400
+
+def test_register_successful_schema_validate():
+    payload = {
+    "email": "eve.holt@reqres.in",
+    "password": "pistol"
+    }
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.post(url + register_successful_endpoint, json=payload, headers=headers)
+    body = response.json()
+    validate(body, schema=schemas.post_register_successful)
+
+def test_single_user_not_found_endpoint_with_no_response_body():
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.get(url + single_user_not_found_endpoint, headers=headers)
+    body = response.json()
+    assert body == {}
+    assert response.status_code == 404
+
+def test_user_update_check_user_params_after_update_and_response_status_cod():
+    name = "morpheus"
+    job = "zion resident"
+    payload = {
+    "name": name,
+    "job": job
+}
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.put(url + user_update_endpoint, json=payload, headers=headers)
+    body = response.json()
+    validate(body, schema=schemas.put_user_update)
+    assert response.status_code == 200
+    assert body["name"] == name
+    assert body["job"] == job
+
+def test_user_delete_check_response_status_code():
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.delete(url + user_delete_endpoint,headers=headers)
+
+    assert response.status_code == 204
+
+def test_user_create_check_response_status_and_user_params_after_create():
+    name = "morpheus"
+    job = "leader"
+    payload = {
+    "name": name,
+    "job": job
+}
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.post(url + user_create_endpoint, json=payload, headers=headers)
+    body = response.json()
+    validate(body, schema=schemas.post_user_create)
+    assert response.status_code == 201
+    assert body["name"] == name
+    assert body["job"] == job
