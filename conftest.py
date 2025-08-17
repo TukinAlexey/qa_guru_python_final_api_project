aea392e7ac40be0db6b20ebed02fd0698cefdd5a
Alexey Tukin <tukin2k@gmail.com>
2025-08-17 20:58:27 +0300

    add_console_log

--- conftest.py
+++ conftest.py
@@ -1,18 +1,21 @@
+from datetime import datetime
 import pytest
-import requests
 
 # Словарь для хранения последних запросов
 _last_responses = {}
 
 @pytest.fixture(scope='function', autouse=True)
-def log_teardown():
+def log_teardown(request):
     yield  # Выполняем сам тест
 
     # Проверяем, есть ли сохранённый ответ для текущего теста
     last_response = _last_responses.get('current_test')
     if last_response is not None:
-        print(f"\nURL: {last_response.request.url}")
-        print(f"Код ответа: {last_response.status_code}")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\nTest name: {request.node.name}")
+        print(f"Current date and time: {current_time}")
+        print(f"Response status code: {last_response.status_code}")
+        print(f"URL: {last_response.request.url}")
 
     # Если ключ существовал, удаляем его
     if 'current_test' in _last_responses:
